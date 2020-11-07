2536bb86fc5dccee3ca936fe5f74162cb20466b9
root <root@localhost.localdomain>
2020-11-07 15:36:11 +0530

    added check_run which checks whether the model is working on the batches of data

--- src/boilerplate/trainer.py
+++ src/boilerplate/trainer.py
@@ -11,48 +11,79 @@ class Trainer:
     self.device = device 
     self.hide_pbar = hide_pbar
 
-  def training_loop(self):
+  def train_step(self, data, label):
+    data, label = data.to(self.device), label.to(self.device)
+    self.optim.zero_grad()
+    out = self.model(data)
+
+    loss = self.criterion(out, label)
+    loss.backward()
+    self.optim.step()
+
+    return out, loss
+
+  def val_step(self, data, label):
+    data, label = data.to(self.device), label.to(self.device)
+    out = self.model(data)
+    loss = self.criterion(out, label)
+
+    return out, loss
+
+  def training_loop(self, dataloader):
     self.model.train()
-    for data, label in tqdm(self.trainloader, total=len(self.trainloader), leave=False, disable=self.hide_pbar):
-      data, label = data.to(self.device), label.to(self.device)
-      self.optim.zero_grad()
-      out = self.model(data)
+    for i, (data, label) in tqdm(enumerate(dataloader), total=len(self.trainloader), leave=False, disable=self.hide_pbar):
+      out, loss = self.train_step(data, label)
 
-      loss = self.criterion(out, label)
-      loss.backward()
-      self.optim.step()
+    return out, loss
 
-    return loss
+  def validation_loop(self, dataloader):
+    with torch.no_grad():
+      for i, (data, label) in tqdm(enumerate(dataloader), total=len(self.valloader), leave=False, disable=self.hide_pbar):
+        out, loss = self.val_step(data, label)
 
-  def validation_loop(self):
-    self.model.eval()
-    for data, label in tqdm(self.valloader, total=len(self.valloader), leave=False, disable=self.hide_pbar):
-      data, label = data.to(self.device), label.to(self.device)
-      out = self.model(data)
-      loss = self.criterion(out, label)
-
-    return loss
+    return out, loss
 
   def check_dataloader(self, dataloader, num_batch):
     for i, (data, label) in enumerate(dataloader):
       print(f'Batch: {i+1}   data_size: {data.shape}   label_size: {label.shape}')
       if (i+1)==num_batch:
-        break
+        break 
 
-  def check_loader(self, num_batches=3):
+  def check_loader(self, n_batches=3):
     print("Training set")
-    self.check_dataloader(self.trainloader, num_batches)
-    
+    self.check_dataloader(self.trainloader, n_batches)
+
     if self.valloader:
       print("\nValidation set")
-      self.check_dataloader(self.valloader, num_batches)
+      self.check_dataloader(self.valloader, n_batches)
 
   def fit(self, epochs: int):
     for epoch in tqdm(range(epochs), total=epochs, leave=False, disable=self.hide_pbar):
-      train_loss = self.training_loop()
+      train_loss = self.training_loop(self.trainloader)[1]
 
       if self.valloader:
-        val_loss = self.validation_loop()
+        val_loss = self.validation_loop(self.valloader)[1]
         print(f'epoch: {epoch+1}\ttrain_loss: {train_loss}\tval_loss: {val_loss}')
       else:
         print(f'epoch: {epoch+1}\ttrain_loss: {train_loss}')
+
+  def predict(self, input_tensor):
+    with torch.no_grad():
+      pred = self.model(input_tensor)
+      return pred
+
+  def check_run(self, n_batch):
+    print("Training")
+    for i, (data_t, label_t) in enumerate(self.trainloader):
+      _, loss_t = self.train_step(data_t, label_t)
+      print(f"Batch: {i+1}  loss: {loss_t}")
+      if (i+1)==n_batch:
+        break
+
+    if self.valloader:
+      print("\nValidating")
+      for j ,(data_v, label_v) in enumerate(self.valloader): 
+        _, loss_v = self.val_step(data_v, label_v)
+        print(f"Batch: {j+1}  loss: {loss_v}")
+        if (j+1)==n_batch:
+          break
--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="boiler-plate-pytorch", 
-    version="0.0.2",
+    version="0.0.3",
     author="Bipin Krishnan P",
     author_email="bipinkrishna.p@gmail.com",
     description="A python package that contains PyTorch boilerplate code for different use cases",
